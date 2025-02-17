7ba4e842f3a3a76a26a3c08a998e84b81f4409f9
remietieventef <remi.etievent.ef@gmail.com>
2025-02-17 19:24:06 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  output: 'standalone', // Permet à Vercel de gérer correctement le build
+  output: 'standalone', // Important pour Vercel
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  },
 };
 
 module.exports = nextConfig;
